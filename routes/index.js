3f8de5e934465d826996fdbf10d0e1a98e771e91
root <root@vps847.directvps.nl>
2012-10-26 07:03:25 +0000

    reasonably finshed. Commit on create dropboxwq

--- routes/index.js
+++ routes/index.js
@@ -6,34 +6,83 @@
 var titlePrefix = {nl: "Accordeonles Den Haag - ", en: "Accordion lessons The Hague - "},
     hostname = "http://vps847.directvps.nl/";
 
-function genericAction(view, title, beforeRender){
+function genericAction(settings){
   this.action = function(req, res){
-    var options = beforeRender ? beforeRender() : {};
-    if(!options.title) options.title = title || '';
+    var options = settings.beforeRender ? settings.beforeRender(settings) : {};
+    if(!options.title) options.title = settings.title || '';
     options.hostname = hostname;
-    if(!options.altpath) res.render(options.view || view, options); 
+    if(settings.otherL) options.otherL = settings.otherL;
+    if(!options.altpath) res.render(options.view || settings.view, options); 
   };
 }
 
 var dutchLookups = {
-  index: new genericAction('indexNL', titlePrefix.nl + 'Home'),
-  bosz: new genericAction('boszNL', titlePrefix.nl + 'Bosz de Kler' ),
-  accordeon: new genericAction('accordeon', titlePrefix.nl + 'Over de accordeon'),
-  volwassenen: new genericAction('volwassenen', titlePrefix.nl + 'Volwassenen'),
-  kinderen: new genericAction('kinderen', titlePrefix.nl + 'Kinderen'),
-  voorwaarden: new genericAction('voorwaarden', titlePrefix.nl + 'Voorwaarden'),
-  tarieven: new genericAction('tarieven', titlePrefix.nl + 'Tarieven'),
-  contact: new genericAction('contactNL', titlePrefix.nl + 'Contact')
+  index: new genericAction({
+    view: 'indexNL',
+    title: titlePrefix.nl + 'Home',
+    otherL: '/en/'}),
+  bosz: new genericAction({
+    view: 'boszNL',
+    title: titlePrefix.nl + 'Bosz de Kler',
+    otherL: '/en/bosz' }),
+  accordeon: new genericAction({
+    view: 'accordeon',
+    title: titlePrefix.nl + 'Over de accordeon',
+    otherL: '/en/accordion'}),
+  volwassenen: new genericAction({
+    view: 'volwassenen',
+    title: titlePrefix.nl + 'Volwassenen',
+    otherL: '/en/adults'}),
+  kinderen: new genericAction({
+    view: 'kinderen',
+    title: titlePrefix.nl + 'Kinderen',
+    otherL: '/en/kids'}),
+  voorwaarden: new genericAction({
+    view: 'voorwaarden',
+    title: titlePrefix.nl + 'Voorwaarden',
+    otherL: '/en/terms'}),
+  tarieven: new genericAction({
+    view: 'tarieven',
+    title: titlePrefix.nl + 'Tarieven',
+    otherL: '/en/prices'}),
+  contact: new genericAction({
+    view: 'contactNL',
+    title: titlePrefix.nl + 'Contact',
+    otherL: '/en/contact'})
 };
 var englishLookups = {
-  index: new genericAction('indexEN' , titlePrefix.en + 'Home'),
-  bosz: new genericAction('boszEN', titlePrefix.en + 'Bosz de Kler'),
-  accordion: new genericAction('accordion', titlePrefix.en + 'the Accordion'),
-  adults: new genericAction('adults', titlePrefix.en + 'Adults'),
-  kids: new genericAction('kids', titlePrefix.en + 'Kids'),
-  terms: new genericAction('terms', titlePrefix.en + 'Terms'),
-  prices: new genericAction('prices', titlePrefix.en + 'Prices'),
-  contact: new genericAction('contactEN', titlePrefix.en + 'Contact')
+  index: new genericAction({
+    view: 'indexEN' ,
+    title: titlePrefix.en + 'Home',
+    otherL: '/nl/'}),
+  bosz: new genericAction({
+    view: 'boszEN',
+    title: titlePrefix.en + 'Bosz de Kler',
+    otherL: '/nl/bosz'}),
+  accordion: new genericAction({
+    view: 'accordion',
+    title: titlePrefix.en + 'the Accordion',
+    otherL: '/nl/accordeon'}),
+  adults: new genericAction({
+    view: 'adults',
+    title: titlePrefix.en + 'Adults',
+    otherL: '/nl/volwassenen'}),
+  kids: new genericAction({
+    view: 'kids',
+    title: titlePrefix.en + 'Kids',
+    otherL: '/nl/kinderen'}),
+  terms: new genericAction({
+    view: 'terms',
+    title: titlePrefix.en + 'Terms',
+    otherL: '/nl/voorwaarden'}),
+  prices: new genericAction({
+    view: 'prices',
+    title: titlePrefix.en + 'Prices',
+    otherL: '/nl/tarieven'}),
+  contact: new genericAction({
+    view: 'contactEN',
+    title: titlePrefix.en + 'Contact',
+    otherL: '/nl/contact'})
 }
 
 exports.nl = {};
